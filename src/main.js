2ea3553cc11a43a76a7abd0f92ea97610c15083b
RafaelMurrieta <r7121005846@gmail.com>
2024-04-04 21:46:51 -0600

    Creacion de index

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/main.css'
+import navbarComponents  from './components/navbar.vue'
+
+
+createApp(navbarComponents).mount('#navbar')
+// createApp(App).mount('#app')
